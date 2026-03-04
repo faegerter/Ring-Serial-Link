0b31831bded8dc4672db28a317e1b0ccfc9ae3cd
fabian <faegerter@student.ethz.ch>
2026-03-04 16:36:27 +0100

    finished unpacker. Commiter and sender aren't finished yet.

--- src/slink_obi_prot_layer.sv
+++ src/slink_obi_prot_layer.sv
@@ -57,7 +57,7 @@ module slink_prot_layer #(
     logic credit_to_send_force;
 
 
-  logic gnt;
+  logic gnt, rvalid;
 
   logic axis_reg_valid_out, axis_reg_ready_out;
   logic axis_reg_valid_in, axis_reg_ready_in;
@@ -66,7 +66,7 @@ module slink_prot_layer #(
 
   always_comb begin : commiter
     gnt  = 1'b0;
-    obi_in_rsp_o.rvalid = 1'b0;
+    obi_in_rsp_o = '0;
     commiter_state_d = commiter_state_q;
     
     unique case(commiter_state_q)
@@ -77,11 +77,15 @@ module slink_prot_layer #(
             commiter_state_d = RWPend;
           end
         end
+        if(obi_out_rsp_i.gnt) begin 
+
+        end
         //TODO add logic to differentiate between cfg registers and axi stream
       end
       RWPend: begin
-          obi_in_rsp_o.rvalid = 1'b1;
-          commiter_state_d = Idle;
+          if(obi_in_rsp_o.rvalid)begin
+            commiter_state_d = Idle;
+          end 
         end
       default:;
     endcase
@@ -96,15 +100,14 @@ end
     payload_out.credit = credits_to_send_q;
 
     if (gnt) begin
-      //TODO ask if this is desired since it uses the same space for reads and writes even though reads are smaller.
       payload_out.axi_ch = obi_in_req_i.a;
       //TODO change slink_pkg to OBI channels
       payload_out.hdr = slink_pkg::TagA;
     end
 
-    // There are three reasons to send out a packet:
+    // There are two reasons to send out a packet:
     // 1) Send out an OBI beat (!TagIdle)
-    // 3) Send an empty packet with credits (credits_to_send_force)
+    // 2) Send an empty packet with credits (credits_to_send_force)
     axis_reg_valid_in = (payload_out.hdr != slink_pkg::TagIdle) | credit_to_send_force;
 
     // There is a potential deadlock situation, when the last credit on the local side
@@ -143,3 +146,104 @@ end
     .ready_i    ( axis_reg_ready_out  ),
     .data_o     ( axis_reg_data_out   )
   );
+
+  logic obi_ch_sent_q, obi_ch_sent_d;
+  logic a_sent_q, a_sent_d;
+  logic r_sent_q, r_sent_d;
+  logic credit_only_packet;
+
+
+  assign a_sent_d   = obi_out_req_o.req & obi_out_rsp_i.gnt |
+                      a_sent_q;
+  assign r_sent_d   = axi_in_rsp_o.r_valid & axi_in_req_i.r_ready |
+                      r_sent_q;
+  assign obi_ch_sent_d = a_sent_d | r_sent_d;
+
+  assign payload_in = payload_t'(axis_in_req_i.t.data);
+  assign credit_only_packet = (payload_in.hdr == slink_pkg::TagIdle);
+
+  always_comb begin : unpacker
+    obi_out_req_o.req = 1'b0;
+    obi_in_rsp_o.rvalid = 1'b0;
+
+    axis_in_rsp_o = '0;
+
+    obi_out_req_o.a = a_chan_t'(payload_in.axi_ch);
+    obi_in_rsp_o.r = r_chan_t'(payload_in.axi_ch);
+
+    unpack_state_d = unpack_state_q;
+
+    // The incoming payload can pack a AW,W,AR,R + an additional B channel
+    // Both channels have to be accepted, if only one of them is accepted
+    // at a time we have to synch
+
+        if (axis_in_req_i.tvalid) begin
+          obi_out_req_o.req = (payload_in.hdr == slink_pkg::TagA);
+          obi_in_rsp_o.rvalid = (payload_in.hdr == slink_pkg::TagR);
+          if (credit_only_packet) begin
+            axis_in_rsp_o.tready = 1'b1;
+          end else begin
+            // accept payload if either one of them was able to send
+            if (obi_ch_sent_d) begin
+              axis_in_rsp_o.tready = 1'b1;
+            end
+          end
+        end
+
+  end
+
+  assign entropy_d = entropy_q + (axis_out_req_o.tvalid & axis_out_rsp_i.tready);
+  `FF(entropy_q, entropy_d, '0)
+  `FF(a_sent_q, a_sent_d, '0)
+  `FF(r_sent_q, r_sent_d, '0)
+  `FF(obi_ch_sent_q, obi_ch_sent_d, '0)
+
+
+  //////////////////////
+  //   FLOW CONTROL   //
+  //////////////////////
+
+  // Flow control is theoretically part of the data link layer.
+  // However it is much simpler to implement it here where we have
+  // simple Handshake interfaces
+
+  always_comb begin
+    credits_out_d = credits_out_q;
+    credits_to_send_d = credits_to_send_q;
+    credit_to_send_force = 1'b0;
+
+    // Send empty packets with credits if there are too many
+    // credits to send but no AXI request transaction
+    if (credits_to_send_q >= ForceSendThresh) begin
+      credit_to_send_force = 1'b1;
+    end
+
+    // The order of the two if blocks matter!
+    if (axis_reg_valid_in & axis_reg_ready_in) begin
+      credits_out_d--;
+      credits_to_send_d = 0;
+    end
+
+    if (axis_in_req_i.tvalid & axis_in_rsp_o.tready) begin
+      credits_out_d += payload_in.credit;
+      credits_to_send_d++;
+    end
+  end
+
+  `FF(credits_out_q, credits_out_d, NumCredits)
+  `FF(credits_to_send_q, credits_to_send_d, 0)
+
+  ////////////////////
+  //   ASSERTIONS   //
+  ////////////////////
+  `ASSERT(AxiComitterAw, axi_in_req_i.w_valid & axi_in_rsp_o.w_ready & axi_in_req_i.w.last
+          |=> $fell(commiter_state_q[1]))
+  `ASSERT(AxiComitterAr, axi_in_rsp_o.r_valid & axi_in_req_i.r_ready & axi_in_rsp_o.r.last
+          |=> $fell(commiter_state_q[0]))
+  `ASSERT(AxisStable, axis_out_req_o.tvalid & !axis_out_rsp_i.tready |=> $stable(axis_out_req_o.t))
+  `ASSERT(AxisHandshake, axis_out_req_o.tvalid & !axis_out_rsp_i.tready |=> axis_out_req_o.tvalid)
+  `ASSERT_INIT(ForceSendTh, ForceSendThresh > 0)
+  `ASSERT(MaxCredits, credits_out_q <= NumCredits)
+  `ASSERT(MaxSendCredits, credits_to_send_q <= NumCredits)
+
+endmodule
